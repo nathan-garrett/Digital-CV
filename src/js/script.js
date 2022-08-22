3496bf4f8f51c2f3fdf63e3653c8b9b8e8d5b3ea
nathan-garrett <nathangarrett@live.co.uk>
2022-08-22 08:13:56 +0100

    responsive css

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
 const tabs = document.querySelectorAll(".qualifications-tab");
 const tabsContainer = document.querySelector(".tabs-container");
 const tabsContent = document.querySelectorAll(".qualification-content");
 
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const allLinks = document.querySelectorAll(".nav-list-item");
+
+//////////////////////////////////////////////////////////////
+// Mobile Navigation
+
+btnNavEl.addEventListener("click", function (e) {
+  headerEl.classList.toggle("nav-open");
+});
+
+//////////////////////////////////////////////////////////////
 // Tabbed content
 
 tabsContainer.addEventListener("click", function (e) {
@@ -20,4 +35,52 @@ tabsContainer.addEventListener("click", function (e) {
   document
     .querySelector(`.qualification-content-${clicked.dataset.tab}`)
     .classList.add("qualification-content-active");
+});
+
+///////////////////////////////////////////////////////////
+//  Sticky Navigation
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the Viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scrolls back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behaviour: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behaviour: "smooth" });
+
+      // close mobile navigation
+      if (link.classList.contains("nav-list-item"))
+        headerEl.classList.toggle("nav-open");
+    }
+  });
 });
